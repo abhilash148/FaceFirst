0747c5522d540dc2b2291f7b25667080516d4bdc
abhilash gudavalli <saiabhilash148@gmail.com>
2022-07-27 01:14:32 +0530

    Camera functionality added

--- FaceFirst/FaceView.swift
+++ FaceFirst/FaceView.swift
@@ -6,3 +6,68 @@
 //
 
 import Foundation
+import UIKit
+import Vision
+
+struct Laser {
+  var origin: CGPoint
+  var focus: CGPoint
+}
+
+class FaceView: UIView {
+  var leftEye: [CGPoint] = []
+  var rightEye: [CGPoint] = []
+  var leftEyebrow: [CGPoint] = []
+  var rightEyebrow: [CGPoint] = []
+  var nose: [CGPoint] = []
+  var outerLips: [CGPoint] = []
+  var innerLips: [CGPoint] = []
+  var faceContour: [CGPoint] = []
+
+  var boundingBox = CGRect.zero
+  
+  func clear() {
+    leftEye = []
+    rightEye = []
+    leftEyebrow = []
+    rightEyebrow = []
+    nose = []
+    outerLips = []
+    innerLips = []
+    faceContour = []
+    
+    boundingBox = .zero
+    
+    DispatchQueue.main.async {
+      self.setNeedsDisplay()
+    }
+  }
+    
+    override func draw(_ rect: CGRect) {
+        guard let context = UIGraphicsGetCurrentContext() else {
+          return
+        }
+        context.saveGState()
+        defer {
+          context.restoreGState()
+        }
+        context.addRect(boundingBox)
+        UIColor.red.setStroke()
+        context.strokePath()
+    }
+}
+
+class LaserView: UIView {
+  private var lasers: [Laser] = []
+  
+  func add(laser: Laser) {
+    lasers.append(laser)
+  }
+  
+  func clear() {
+    lasers.removeAll()
+    DispatchQueue.main.async {
+      self.setNeedsDisplay()
+    }
+  }
+}
--- FaceFirst/ViewController.swift
+++ FaceFirst/ViewController.swift
@@ -6,14 +6,121 @@
 //
 
 import UIKit
+import AVFoundation
+import Vision
+import Foundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    @IBOutlet var faceView: FaceView!
+    @IBOutlet var laserView: LaserView!
+    @IBOutlet var faceLaserLabel: UILabel!
+    
+    let session = AVCaptureSession()
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    
+    let dataOutputQueue = DispatchQueue(
+        label: "video data queue",
+        qos: .userInitiated,
+        attributes: [],
+        autoreleaseFrequency: .workItem)
+    
+    var faceViewHidden = false
+    var sequenceHandler = VNSequenceRequestHandler()
+    
+    var maxX: CGFloat = 0.0
+    var midY: CGFloat = 0.0
+    var maxY: CGFloat = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureCaptureSession()
+        laserView.isHidden = true
+        maxX = view.bounds.maxX
+        midY = view.bounds.midY
+        maxY = view.bounds.maxY
+        
+        session.startRunning()
     }
-
-
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        guard let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+          return
+        }
+        let detectFaceRequest = VNDetectFaceRectanglesRequest(completionHandler: detectedFace)
+        do {
+          try sequenceHandler.perform(
+            [detectFaceRequest],
+            on: imageBuffer,
+            orientation: .leftMirrored)
+        } catch {
+          print(error.localizedDescription)
+        }
+    }
+    
+    @IBAction func handleTap(_ sender: UITapGestureRecognizer) {
+        faceView.isHidden.toggle()
+        laserView.isHidden.toggle()
+        faceViewHidden = faceView.isHidden
+        
+        if faceViewHidden {
+            faceLaserLabel.text = "Lasers"
+        } else {
+            faceLaserLabel.text = "Face"
+        }
+    }
+    
+    func configureCaptureSession() {
+        guard let camera = AVCaptureDevice.default(.builtInWideAngleCamera,
+                                                   for: .video,
+                                                   position: .front) else {
+            return
+        }
+        
+        do {
+            let cameraInput = try AVCaptureDeviceInput(device: camera)
+            session.addInput(cameraInput)
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+        
+        let videoOutput = AVCaptureVideoDataOutput()
+        videoOutput.setSampleBufferDelegate(self, queue: dataOutputQueue)
+        videoOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA]
+        
+        session.addOutput(videoOutput)
+        self.setVideoConnection(videoOutput)
+    }
+    
+    func setVideoConnection(_ output: AVCaptureVideoDataOutput) {
+        let videoConnection = output.connection(with: .video)
+        videoConnection?.videoOrientation = .portrait
+        previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer.videoGravity = .resizeAspectFill
+        previewLayer.frame = view.bounds
+        view.layer.insertSublayer(previewLayer, at: 0)
+    }
+    
+    func convert(rect: CGRect) -> CGRect {
+      let origin = previewLayer.layerPointConverted(fromCaptureDevicePoint: rect.origin)
+        let size = previewLayer.layerPointConverted(fromCaptureDevicePoint: rect.origin)
+      return CGRect(origin: origin, size: CGSize())
+    }
+    
+    func detectedFace(request: VNRequest, error: Error?) {
+      guard
+        let results = request.results as? [VNFaceObservation],
+        let result = results.first
+        else {
+          faceView.clear()
+          return
+      }
+      let box = result.boundingBox
+      faceView.boundingBox = convert(rect: box)
+      DispatchQueue.main.async {
+        self.faceView.setNeedsDisplay()
+      }
+    }
 }
 
